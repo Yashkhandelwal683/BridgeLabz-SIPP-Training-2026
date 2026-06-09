cf961645d25cc9be2c91a6088236c0e7f66f1a8d
Yashkhandelwal683 <khandelwalyash355@gmail.com>
2026-06-09 09:56:19 +0530

    Added level1.java

---
+++ gcr_codebase/Programming_Elements/level1/level1.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class level1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        int recentyear = 2024;
+        int age = recentyear - year;
+        System.out.println("Harry age in year is " + age);
+    }
+}
